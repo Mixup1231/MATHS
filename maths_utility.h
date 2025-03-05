b51946ab9b4cadc4f41f7ab37c8b3960c13a44cc
Mixup1231 <110330912+Mixup1231@users.noreply.github.com>
2025-03-05 12:13:29 +1000

    Update maths_utility.h

--- maths_utility.h
+++ maths_utility.h
@@ -4,8 +4,8 @@
 #include "vector.h"
 
 #define PI 3.1415926535898
-#define to_radians(x) ((x * PI) / 180)
-#define to_degrees(x) ((x * 180) / PI)
+#define to_radians(x) (((x) * PI) / 180)
+#define to_degrees(x) (((x) * 180) / PI)
 
 Vec3f to_direction(const Vec3f& euler);
 Vec3f to_euler(const Vec3f& direction);
